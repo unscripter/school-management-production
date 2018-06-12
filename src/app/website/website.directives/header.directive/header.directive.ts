0697fe04a3440520753c6569df37f5aa8ed8f386
Amit Mundra <amit.mundra52@gmail.com>
2018-06-13 02:05:07 +0530

    website header is designed

--- src/app/website/website.directives/header.directive/header.directive.ts
+++ src/app/website/website.directives/header.directive/header.directive.ts
@@ -7,6 +7,13 @@ import { Component } from '@angular/core';
 
 export class WebsiteHeader {
 constructor() {
-
+  this.navItemList = [
+    { name: "About Us", class: "nav-link active" },
+    { name: "Admissions", class: "nav-link active" },
+    { name: "Academics", class: "nav-link active" },
+    { name: "Blog", class: "nav-link active" },
+    { name: "Research", class: "nav-link active" },
+    { name: "Contact Us", class: "nav-link active" }
+  ];
 }
 }
